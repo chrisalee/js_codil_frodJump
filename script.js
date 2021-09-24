748aeea39afe2926268e3666c17813c176a4df0b
Christopher L <56335217+chrisalee@users.noreply.github.com>
2021-09-24 09:46:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const solution = (start, finish, jump) =>  Math.ceil((finish - start) / jump);
+
+
+// const solution = (X, Y, D) => {
+//   let count = 0;
+//   let position = X;
+//   while(position < Y) {
+//     count++;
+//     position + D;
+//   }
+//   return count;
+// }
